be0d94fa9d5367c03e51dff6050c110bba743325
Mireia <66952824+miiree12lb@users.noreply.github.com>
2025-01-20 14:47:47 +0100

    redirect on recharging the page

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,7 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Item({item}) {
     const [selectedCode, setSelectedCode] = useState("jsx");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+          // Check for F5 or Ctrl+R
+          if (event.key === 'F5' || (event.ctrlKey && event.key === 'r')) {
+            event.preventDefault(); // Prevent the default browser refresh
+            navigate('/'); // Redirect to "/"
+          }
+        };
+    
+        window.addEventListener('keydown', handleKeyDown);
+
+        if (!item) {
+            navigate("/");
+        }
+    
+        // Cleanup the event listener on component unmount
+        return () => {
+          window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [navigate]);
 
     return (
         <div className="item-page">
